44139d1da2482e47e147515275e883c220affba9
Marco Carnaghi <125691581+MarcoCarnaghiCoding@users.noreply.github.com>
2023-09-06 15:55:01 -0300

    Added en_core model

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #import pandas as pd
-#import spacy
+import spacy
 import en_core_web_sm
 from gensim.corpora.dictionary import Dictionary
 from gensim.models import LdaMulticore
@@ -8,7 +8,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+#!pip install ./nlp_model/en_core_web_sm-3.0.0.tar.gz
 
 
 def get_topics(review_text=''):
@@ -27,7 +27,11 @@ def get_topics(review_text=''):
     9. Customer Support, enjoyable atmosphere and Afforable
     '''
     # Load the spaCy model
-    nlp = en_core_web_sm.load()
+    try:
+        nlp = nlp = en_core_web_sm.load()
+    except:
+        # Load the spaCy model
+        nlp = spacy.load('./en_core_web_sm-3.0.0/en_core_web_sm/en_core_web_sm-3.0.0')
 
     # Tags you want to remove from the text
     removal = ['ADV', 'PRON', 'CCONJ', 'PUNCT', 'PART', 'DET', 'ADP', 'SPACE', 'NUM', 'SYM', 'SCONJ', 'ADP', 'ADJ']
